d237033922f538962f28840638020bbe60f50b4c
Axel Gard <axel.gard@protonmail.com>
2025-05-20 16:18:08 +0200

    added work on data collection

--- data/get_data_sel.py
+++ data/get_data_sel.py
@@ -32,18 +32,23 @@ class WebScraper:
         self.driver.close()
 
 def get_lat_lng(address:str):
-    API_KEY = "AIzaSyAaIHU2ZTditFtPL-K05lt4XDtiWMrtaKk" 
+    API_KEY = "AIzaSyCSGDtR0WqTmCdzgAC4bxrN7hliAXZM14M" 
     url = f"https://maps.googleapis.com/maps/api/geocode/json?address={address}&key={API_KEY}"
     response = requests.get(url)
     data = response.json()
-    print(data)
     location = data["results"][0]["geometry"]["location"]
     sleep(0.5)
     return {
-        "lat":location["latitude"],
-        "lng":location["longitude"],
+        "lat":location["lat"],
+        "lng":location["lng"],
         "alt": 0.0
     }
+
+def same_len(dic:dict):
+    _len = len(list(dic.values())[0])
+    for k, v in dic.items():
+        if _len != len(v): return False, k
+    return True, None
 
 def main():
     
@@ -57,11 +62,12 @@ def main():
         "size": [],
         "type": [],
         "date": [],
-        "lat": [],
-        "lng": [],
     }
 
-    for listing_idx, listing in tqdm(enumerate(ws.soup.find_all(class_="object-card-layout__content")[:2])):
+    lat = []
+    lng = []
+
+    for listing_idx, listing in tqdm(enumerate(ws.soup.find_all(class_="object-card-layout__content"))):
         tags = str(listing)
         tags = unicodedata.normalize("NFKD",tags).split(">")
         #pprint(tags)
@@ -70,7 +76,6 @@ def main():
                 to_df["street_name"].append(tags[idx+1].replace("</a", ""))
             elif t == '<span class="object-card__preamble"':
                 area = tags[idx+1].replace("</span", "").split(" · ")
-                print(area)
                 to_df["type"].append(area[0])
                 to_df["area"].append(area[1])
                 if len(area) == 3:
@@ -84,30 +89,42 @@ def main():
                 p = p.replace('kr</span', "").replace(" ", "")
                 p = float(p)
                 to_df["price"].append(p)
-            elif '<li aria-label="' in t and "m2" in tags[idx+1] and not "tomt" in tags[idx+1] and not "kr" in tags[idx+1]:
+            elif '<li aria-label="' in t and "m2" in tags[idx+1] and not ("tomt" in tags[idx+1]) and not ("kr" in tags[idx+1]):
                 msq = tags[idx+1].replace("m2", "").replace(" ", "").replace(",", ".").replace("</li", "")
                 if '+'in msq:
                     msq = sum([float(m) for m in msq.split("+")])
                 msq = float(msq)
                 to_df["size"].append(msq)
-
-        address = f'{to_df["street_name"][listing_idx]}, {to_df["city"][listing_idx]}'
-        location = get_lat_lng(address)
-        to_df["lat"].append(location["lat"])
-        to_df["lng"].append(location["lng"])
+        skipp = False
+        if not same_len(to_df)[0]:
+            #pprint(tags)
+            key_missing = same_len(to_df)[1]
+            for k, v in to_df.items():
+                if len(v) and k != key_missing:
+                    to_df[k].pop(-1)
+            #pprint([(k,len(v)) for k,v in to_df.items()])
+            skipp = True
+            continue
+            
+        if not skipp:
+            address = f'{to_df["street_name"][-1]}, {to_df["city"][-1]} sweden'
+            location = get_lat_lng(address)
+            lat.append(location["lat"])
+            lng.append(location["lng"])
+            
+    to_df["lat"] = lat
+    to_df["lng"] = lng
+    try:
+        df = pd.DataFrame(to_df)
+        df = df.loc[df["lat"] != 0]
+        df.reset_index(inplace=True)
+        print(df)
+    except:
+        pprint([(k,len(v)) for k,v in to_df.items()])
             
 
-    df = pd.DataFrame(to_df)
-    print(df)
-    #df["alt"] = 0.0
-    #df = df.loc[df["lat"] != 0]
-    #df.reset_index(inplace=True)
-    #print(df)
-            
 
 
-
-    del ws
 
 
 if __name__ == "__main__":
